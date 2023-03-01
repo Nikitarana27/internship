31474242082ba325bc92cd28d04fa37f2e965a53
Nikitarana27 <rnikita445@gmail.com>
2023-03-01 09:14:11 +0530

    First Commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,22 @@
 import React , {useState} from 'react';
-import {Employees} from './Empolyees';
+// import {Employees} from './Empolyees';
 
 export default function Header() {
 const [input,setInput]=useState(" ");
-const search_data =(input_value) =>{
-  // console.log("hey");
-  var results = [];
+// const search_data =(input_value) =>{
+//   // console.log("hey");
+//   var results = [];
 
 
 
-for(var i=0; i<Employees.length; i++) {
-  for(key in Employees[i]) {
-    if(Employees[i][key].indexOf(input_value)!=-1) {
-      results.push(Employees[i]);
-    }
-  }
-}
-}
+// for(var i=0; i<Employees.length; i++) {
+//   for(key in Employees[i]) {
+//     if(Employees[i][key].indexOf(input_value)!=-1) {
+//       results.push(Employees[i]);
+//     }
+//   }
+// }
+// }
 
   return (
     <div>
@@ -25,7 +25,7 @@ for(var i=0; i<Employees.length; i++) {
           <a href="\" className="navbar-brand" style={{color: "white"}}>Employee Details</a>
           <form className="d-flex" role="search">
             <input className="form-control me-2" type="search" value={input} placeholder="Search" aria-label="Search" onChange={(e)=>setInput(e.target.value)}/>
-              <button className="btn btn-outline-success" type="submit" onClick={search_data(input)}>Search</button>
+              <button className="btn btn-outline-success" type="submit" >Search</button>
           </form>
         </div>
       </nav>
